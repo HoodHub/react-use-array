9f75a20084598708a1794ccafb57f708372326dc
isaiahtaylor <isaiah@hoodhub.com>
2020-05-06 14:38:36 -0700

    converted to ts

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { updateAt } from './utils';
+
+const useArray = <T>(initialList: T[]) => {
+  const [list, set] = useState(initialList);
+  return [
+    list,
+    {
+      set,
+      empty: () => set([]),
+      replace: (list: T[]) => set(list),
+      push: (item: T) => set(l => [...l, item]),
+      updateAt: (index: number, updateFn: (item: T) => T) => updateAt(index, updateFn, set),
+      setAt: (index: number, value: T) => set(l =>
+        [...l.slice(0, index), value, ...l.slice(index + 1)]
+      ),
+      removeAt: (index: number) => set(l => [...l.slice(0, index), ...l.slice(index + 1)]),
+      filter: (filterFn: (item: T) => boolean) => set(l => l.filter(filterFn)),
+      map: (mapFn: (item: T) => T) => set(l => [...l].map(mapFn)),
+      sort: (sortFn: (a: T, b: T) => number) => set(l => [...l].sort(sortFn)),
+      reverse: () => set(l => [...l].reverse()),
+      mergeBefore: (arr: T[]) => set(l => [...arr].concat([...l])),
+      mergeAfter: (arr: T[]) => set(l => [...l].concat([...arr])),
+    }
+  ];
+}
+
+export default useArray;
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-export const updateAt = (index, updateFn, set) =>
+export const updateAt = <T>(index: number, updateFn: (item: T) => T, set: React.Dispatch<React.SetStateAction<T[]>>) =>
   set(l => {
     const copy = l.slice(0);
     const item = copy[index];
